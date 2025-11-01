4f92c1c60e36be70409a005018279623997b5d30
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2025-11-01 13:50:56 -0500

    Create an initial setup to filter tasks

--- src/dashboard/main.py
+++ src/dashboard/main.py
@@ -2,8 +2,8 @@ from dash import Dash, html
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-def create_df():
-  df = pd.read_csv("./omnifocus.csv")
+def create_raw_df(filepath):
+  df = pd.read_csv(filepath)
   df = df.drop("Context", axis=1)
   df = df.drop("Completion Date", axis=1)
   df = df.drop("Duration", axis=1)
@@ -14,60 +14,97 @@ def create_df():
   df = df.drop("Status", axis=1)
   df = df.drop("Tags", axis=1)
   df = df.drop("Task ID", axis=1)
+  df['Due Date'] = pd.to_datetime(df['Due Date'])
   return df
 
-def urgent_and_important():
+def remove_unneeded_entries(df):
+  unneeded_entries = [
+    "Get to Inbox Zero for Discord Messages",
+    "Get to Inbox Zero for Emails",
+    "Get to Inbox Zero for Slack",
+    "Get to Inbox Zero for Voicemail Messages",
+    "Get to Inbox Zero for What’s App Messages",
+    "Get to Inbox Zero for Text Messages",
+    "Get to Inbox Zero for Microsoft Teams",
+    "Every Week",
+    "Every Month",
+    "Pack Lunches",
+    "February",
+    "October",
+    "November"
+  ]
+  return df[df["Name"].isin(unneeded_entries) == False]
+
+def urgent_condition(df):
+  return (df['Due Date'].notnull() & (df['Due Date'] < "2025-12-01"))
+
+def important_condition(df):
+  return (df['Name'].notnull())
+
+def urgent(df):
+  return df.where(urgent_condition(df))
+
+def not_urgent(df):
+  return df.mask(urgent_condition(df))
+
+def important(df):
+  return df.where(important_condition(df))
+
+def not_important(df):
+  return df.mask(important_condition(df))
+
+def urgent_and_important(df):
+  fdf = urgent(important(df))
+  tasks = fdf["Name"].map(lambda x: html.P(x))
   return [
     html.H3("Urgent & Important (Do)", className="text-center"),
-    html.Div(id="q1-tasks", className="p-2 border", children=[
-      html.P("Hello")
-    ]),
+    html.Div(id="q1-tasks", className="p-2 border", children=tasks),
   ]
 
-def urgent_not_important():
+def urgent_not_important(df):
+  fdf = urgent(not_important(df))
+  tasks = fdf["Name"].map(lambda x: html.P(x))
   return [
     html.H3("Urgent but Not Important (Delegate)", className="text-center"),
-    html.Div(id="q3-tasks", className="p-2 border", children=[
-      html.P("Hello")
-    ]),
+    html.Div(id="q3-tasks", className="p-2 border", children=tasks),
   ]
 
-def important_not_urgent():
+def important_not_urgent(df):
+  fdf = not_urgent(important(df))
+  tasks = fdf["Name"].map(lambda x: html.P(x))
   return [
     html.H3("Important but Not Urgent (Schedule)", className="text-center"),
-    html.Div(id="q2-tasks", className="p-2 border", children=[
-      html.P("Hello")
-    ])
+    html.Div(id="q2-tasks", className="p-2 border", children=tasks)
   ]
 
-def not_urgent_not_important():
+def not_urgent_not_important(df):
+  fdf = not_urgent(not_important(df))
+  tasks = fdf["Name"].map(lambda x: html.P(x))
   return [
       html.H3("Not Urgent & Not Important (Eliminate)", className="text-center"),
-      html.Div(id="q4-tasks", className="p-2 border", children=[
-      html.P("Hello")
-    ])
+      html.Div(id="q4-tasks", className="p-2 border", children=tasks)
   ]
 
-def create_app():
+def create_app(df):
   app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
   app.layout = dbc.Container([
       html.H1("Eisenhower Matrix", className="text-center my-4"),
       dbc.Row([
-          dbc.Col(html.Div(urgent_and_important()), md=6),
-          dbc.Col(html.Div(important_not_urgent()), md=6),
+          dbc.Col(html.Div(urgent_and_important(df)), md=6),
+          dbc.Col(html.Div(important_not_urgent(df)), md=6),
       ]),
       dbc.Row([
-          dbc.Col(html.Div(urgent_not_important()), md=6),
-          dbc.Col(html.Div(not_urgent_not_important()), md=6),
+          dbc.Col(html.Div(urgent_not_important(df)), md=6),
+          dbc.Col(html.Div(not_urgent_not_important(df)), md=6),
       ])
   ], fluid=True)
   return app
 
 
 def start():
-  df = create_df()
-  print(df)
-  app = create_app()
+  df = create_raw_df("./omnifocus.csv")
+  df = remove_unneeded_entries(df)
+  app = create_app(df)
   app.run(debug=True)
 
 if __name__ == '__main__':
