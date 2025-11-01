a072d5cc71ec0033989cab7eea36b8dba1f19d5b
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2025-11-01 11:28:53 -0500

    Filter out unused columns

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ poetry install
 ## Setting Up Data
 
 1. Open Omnifocus
-2. Select "Everything" under "View"
+2. Select "Remaining" under "View"
 3. Export a CSV document with projects and tasks using File > Export...
 4. Save the exported CSV document to the root of this project as "omnifocus.csv"
 
--- src/dashboard/main.py
+++ src/dashboard/main.py
@@ -2,6 +2,16 @@ import pandas as pd
 
 def start():
   df = pd.read_csv("./omnifocus.csv")
+  df = df.drop("Context", axis=1)
+  df = df.drop("Completion Date", axis=1)
+  df = df.drop("Duration", axis=1)
+  df = df.drop("Flagged", axis=1)
+  df = df.drop("Planned Date", axis=1)
+  df = df.drop("Notes", axis=1)
+  df = df.drop("Start Date", axis=1)
+  df = df.drop("Status", axis=1)
+  df = df.drop("Tags", axis=1)
+  df = df.drop("Task ID", axis=1)
   print(df)
 
 if __name__ == '__main__':
